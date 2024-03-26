0f16a32d63549eabe216999b0b46d02ee10eec05
coco3x <inferior3x@gmail.com>
2024-03-26 21:38:43 +0900

    init: 프로젝트 생성 및 의존성 추가

---
+++ src/main/java/com/econovation/teamponyo/TeamPonyoApplication.java
@@ -0,0 +1,11 @@
+package com.econovation.teamponyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPonyoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamPonyoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/econovation/teamponyo/TeamPonyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econovation.teamponyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPonyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
