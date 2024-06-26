35309a44a3c53d0fe77385a3268f41f361f9e74e
coco3x <inferior3x@gmail.com>
2024-06-26 19:07:50 +0900

    add: 공용 문자열을 다루는 클래스

---
+++ src/main/java/com/econovation/teamponyo/common/consts/CommonStatics.java
@@ -0,0 +1,23 @@
+package com.econovation.teamponyo.common.consts;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommonStatics {
+    public static final String ONJEON = "onjeon";
+
+    //JWT
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String OAUTH2_LOGIN_TOKEN = "oauth2_login_token";
+    public static final String TOKEN_TYPE = "token_type";
+    public static final String EMAIL = "email";
+    public static final String SOCIAL_PROVIDER = "social_provider";
+    public static final String USER_ID = "user_id";
+    public static final String TOKEN_ID = "token_id";
+    public static final String SOCIAL_ID = "social_id";
+    public static final String ACCOUNT_TYPE = "account_type";
+    public static final String IS_REGISTERED = "is-registered";
+    public static final String AUTHORIZATION = "Authorization";
+}
